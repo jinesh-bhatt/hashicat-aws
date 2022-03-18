23397deddaa36dacc9c27f400887fe57089d4212
Jinesh Bhatt <jinesh_bhatt@vanguard.com>
2022-03-18 18:47:33 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,8 @@
 module "s3_bucket" {
   source  = "app.terraform.io/jin/s3-bucket/aws"
   version = "2.8.0"
-
+  bucket_prefix = "jine-test"
+  prefix = "jinesh-bhatt"
   bucket = "my-s3-bucket"
   acl    = "private"
 
